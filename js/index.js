2154cd93627e44778dc8c8568e42556c271fd6c6
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-05-15 20:57:43 +0900

    ログイン サインアップ ボタンのdisableを無効にする

--- js/index.js
+++ js/index.js
@@ -10,3 +10,30 @@ $(".messagearea").on("input", function () {
     $("#sendbtn").prop("disabled", true); //disabled を有効にする＝ボタンが押せない
   }
 });
+
+$(".liarea").on("input", function () {
+  var input1 = $(liuser).val(); //#liuser に入力された文字を取得
+  var input2 = $(lipass).val(); //#lipass に入力された文字を取得
+
+  if (input1 && input2) {
+    //もし文字が入っていれば
+
+    $("#loginbtn").prop("disabled", false); //disabled を無効にする＝ボタンが押せる
+  } else {
+    $("#loginbtn").prop("disabled", true); //disabled を有効にする＝ボタンが押せない
+  }
+});
+
+$(".suarea").on("input", function () {
+  var input1 = $(suuser).val(); //#suuser に入力された文字を取得
+  var input2 = $(supass).val(); //#supass に入力された文字を取得
+  var input3 = $(supass2).val(); //#supass2 に入力された文字を取得
+
+  if (input1 && input2 && input3 && input2 == input3) {
+    //もし文字が入っていれば
+
+    $("#signupbtn").prop("disabled", false); //disabled を無効にする＝ボタンが押せる
+  } else {
+    $("#signupbtn").prop("disabled", true); //disabled を有効にする＝ボタンが押せない
+  }
+});
